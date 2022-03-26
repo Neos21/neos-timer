e4b5d32e7d44d4396abd1cccf8c12dcc16e6591c
Neos21 <neos21@gmail.com>
2022-03-27 00:15:25 +0900

    v1

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Neo's Timer
 
 - __[Enter The WebSite](https://neos21.github.io/neos-timer/)__
+  - Font made from [oNline Web Fonts](http://www.onlinewebfonts.com) is licensed by CC BY 3.0
+  - BGM by [OtoLogic](https://otologic.jp) (CC BY 4.0)
 
 
 ## Links
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -6,7 +7,8 @@ import { AppComponent } from './app.component';
 /** App Module */
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   bootstrap: [
     AppComponent
